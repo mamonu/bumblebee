0e605595814477c797306f07a23c5e999150cb7e
Ale Tosi <al.to@live.com>
2018-08-28 21:48:57 +0100

    added transformer to use custom featurizers within sklearn pipeline

--- nlpfunctions/utils.py
+++ nlpfunctions/utils.py
@@ -8,7 +8,7 @@ Created on Mon Jul  9 13:36:32 2018
 
 
 import pandas as pd
-
+import numpy as np
 
 
 
@@ -51,7 +51,7 @@ def string_to_series_out(func):
     
 import functools
 
-# Ref. for functolls.reduce: # https://docs.python.org/3/library/functools.html#functools.reduce
+# Ref. for functools.reduce: # https://docs.python.org/3/library/functools.html#functools.reduce
     
   
 def combine_2fs(f, g):
@@ -129,6 +129,16 @@ def list2string(list_of_strings) :
 import collections
 
 def flattenIrregularListOfLists(l):
+    """ 
+    Function to flatten a list of lists that is nested /irregular  eg  [1,2,[],[[3]]],4,[5,6]]
+    Returns a flattened list generator 
+    
+    Parameters
+    ----------
+    INPUT : a ( nested) list of lists 
+    OUTPUT : a flattened list generator 
+    
+    """
     for el in l:
         if isinstance(el, collections.Iterable) and not isinstance(el, (str,bytes)):
             yield from flattenIrregularListOfLists(el)
@@ -138,7 +148,147 @@ def flattenIrregularListOfLists(l):
  
     
     
+
+###########################################################################################
+#### Custom Transformers to apply chain of function featurizers in a sklearn pipeline #####
+###########################################################################################
+
+# References: 
+# https://www.slideshare.net/PyData/julie-michelman-pandas-pipelines-and-custom-transformers
+# http://fastml.com/converting-categorical-data-into-numbers-with-pandas-and-scikit-learn/
+
+# Custom Transformers
+
+# NOTE: BaseEstimator is included to inherit get_params() which is needed for Grid Search
+            
+
+from sklearn.base import BaseEstimator, TransformerMixin
     
+class list2array_TextFunctionFeaturizer(BaseEstimator, TransformerMixin):
+    """
+    https://dreisbach.us/articles/building-scikit-learn-compatible-transformers/
+    Takes a list of functions, calls each function with our text (X as list of strings), and 
+    returns the results of all functions as a feature vector as np.array
+    """
+    
+    def __init__(self, *featurizers):
+        self.featurizers = featurizers
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        """Given a list of original data, return an array of list of feature vectors."""
+        fvs = []
+        for datum in X:
+            fv = [f(datum) for f in self.featurizers]
+            fvs.append(fv)
+        return np.array(fvs)
+
+
+
+class list2list_TextFunctionFeaturizer(BaseEstimator, TransformerMixin):
+    """
+    Modified from:
+    https://dreisbach.us/articles/building-scikit-learn-compatible-transformers/
+    Takes a list of functions, calls each function with our list of lists (X), 
+    and returns the results of all functions as a feature vector as an np.array.
+    """
+    
+    def __init__(self, *featurizers):
+        self.featurizers = featurizers
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        """Given a list of lists of original data, return a list of feature vectors."""
+        fvs = []
+        for datum in X:
+            [fv] = [f(datum) for f in self.featurizers]
+            fvs.append(fv)
+        return np.array(fvs)
+
+
+
+
+class ColumnSelector(BaseEstimator, TransformerMixin):
+    """
+    Class for building sklearn Pipeline step. 
+    This class selects a column from a pandas data frame.
+    """
+    
+    # initialise
+    def __init__(self, columns):
+        self.columns = columns          # e.g. pass in a column name to extract
+
+    def fit(self, df, y=None): 
+        return self                     # does nothing
+
+    def transform(self, df):            #df: dataset to pass to the transformer
+        
+        df_cols = df[self.columns]    #equivelent to df['columns']
+        return df_cols
+
+    
+    
+class CatToDictTransformer(BaseEstimator, TransformerMixin):
+    """
+    Class for building sklearn Pipeline step. 
+    This class turns columns from a pandas data frame (type Series) that
+    contain caegorical variables into a list of dictionaries 
+    that can be inputted into DictVectorizer().
+    """
+    
+    # initialise
+    def __init__(self):
+        self              #could also use 'pass'
+        
+    # 
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):        #X: dataset to pass to the transformer.
+        Xcols_df = pd.DataFrame(X)
+        Xcols_dict = Xcols_df.to_dict(orient = 'records')
+        return Xcols_dict
+
+
+import itertools
+
+class Series2ListOfStrings(BaseEstimator, TransformerMixin):
+    
+    """
+    Class for building sklearn Pipeline step. 
+    This class turns columns from a pandas data frame (type Series) that
+    contain lists of string sentences into a list of strings 
+    that can be inputted into CountVectorizer().
+    """
+    # initialise
+    def __init__(self):
+        self              
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):        #X: dataframe to pass to the transformer.
+        Xvals = X.values
+        Xstrs = list(itertools.chain.from_iterable(Xvals))   #flatten nested list
+        return Xstrs
+
+
+class DenseTransformer(TransformerMixin):
+
+    def transform(self, X, y=None, **fit_params):
+        return X.todense()
+
+    def fit_transform(self, X, y=None, **fit_params):
+        self.fit(X, y, **fit_params)
+        return self.transform(X)
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
 
 
 
