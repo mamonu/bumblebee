a174e086be280f35805de15868636338ea1e2d36
MichaelaLawrenceONS <michaela.lawrence@ons.gov.uk>
2019-01-08 17:19:39 +0000

    Added a noun-phrases extraction function, and a very simple example of use.

---
+++ nlpbumblebee/nounphrases.py
@@ -0,0 +1,13 @@
+from textblob import TextBlob
+import nltk
+
+def extract_noun_phrases(text):
+	stop_words = set(nltk.corpus.stopwords.words("english")) # to remove stopwords
+	blob = TextBlob(text) # create a textblob object 
+	np = blob.noun_phrases # selecting all noun-phrases
+	np = [w for w in np if len(w)>=3] # remove nounphrses that are less than 3 characters
+	nounps = [
+		word.lower()
+		for word in np
+		if word.lower() not in stop_words]
+	return nounps
